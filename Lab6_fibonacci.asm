99959a13f917377679c586ab41aefd750d494bdd
Nik <nik.grape@gmail.com>
2025-05-12 16:34:34 +0200

    Lab 6 done, tests passed

--- Lab6_fibonacci.asm
+++ Lab6_fibonacci.asm
@@ -5,16 +5,32 @@
 # size: s0
 # address: s1
 
-addi $t0, $zero, 0
-addi $t1, $zero, 1
-add $t2, $zero, $s1
+addi $t0, $zero, 1 # coutner
+
+addi $t1, $zero, 1 # prev
+addi $t2, $zero, 1 # current
+addi $t3, $zero, 1 # next
+
+sw $zero, 0($s1)             # save zero number to address
+sw $t1, 4($s1)               # save first number to address
+addi $t4, $s1, 8             # increase address on 8 and save it to t1
+
 
 # t0 - loop counter
-# t1 - current fibonacci number
-# t2 - current address in memory
+# t1 - prev fibonacci number
+# t2 - current fibonacci number
+# t3 - next term
+# t4 - current address in memory
 Loop: beq $t0, $s0, Exit        # while (t0 != s0)
-    sw $t1, 0($t2)              # save t1 to memory
-    add
+    sw $t3, 0($t4)              # save next_term to memory
+
+    add $t1, $t2, $zero         # prev = current
+    add $t2, $t3, $zero         # currnt = next_term
+    add $t3, $t1, $t2           # next_term = prev + current
+
+    addi $t0, $t0, 1             # loop counter += 1
+    addi $t4, $t4, 4             # increase address on 4
+
     j Loop
 Exit:
     
