d9ad398cb721d09930981b19859e86bf770f1414
Nik <nik.grape@gmail.com>
2025-05-13 10:21:00 +0200

    lab 6 cleanup

--- Lab6_fibonacci.asm
+++ Lab6_fibonacci.asm
@@ -2,25 +2,26 @@
 
 
 
-# size: s0
-# address: s1
+# size: $s0
+# memory address: $s1
+addi $t0, $zero, 1          # coutner
+addi $t1, $zero, 1          # prev
+addi $t2, $zero, 1          # current
+addi $t3, $zero, 1          # next
 
-addi $t0, $zero, 1 # coutner
+sw $zero, 0($s1)            # save first term to memory
+beq $s0, $t1, Exit          # if size == 1 end the program
 
-addi $t1, $zero, 1 # prev
-addi $t2, $zero, 1 # current
-addi $t3, $zero, 1 # next
-
-sw $zero, 0($s1)             # save zero number to address
-sw $t1, 4($s1)               # save first number to address
-addi $t4, $s1, 8             # increase address on 8 and save it to t1
+sw $t1, 4($s1)              # save second term to memory
+addi $t4, $s1, 8            # increase address on 8 and save it to t1
 
 
 # t0 - loop counter
-# t1 - prev fibonacci number
-# t2 - current fibonacci number
+# t1 - prev term
+# t2 - current term
 # t3 - next term
 # t4 - current address in memory
+
 Loop: beq $t0, $s0, Exit        # while (t0 != s0)
     sw $t3, 0($t4)              # save next_term to memory
 
@@ -28,8 +29,8 @@ Loop: beq $t0, $s0, Exit        # while (t0 != s0)
     add $t2, $t3, $zero         # currnt = next_term
     add $t3, $t1, $t2           # next_term = prev + current
 
-    addi $t0, $t0, 1             # loop counter += 1
-    addi $t4, $t4, 4             # increase address on 4
+    addi $t0, $t0, 1            # loop counter += 1
+    addi $t4, $t4, 4            # increase address on 4
 
     j Loop
 Exit:
